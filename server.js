0a892d18e38f07df1cc06c8b78bfb65e1419697d
greg <grzegorz.smolin@agricircle.com>
2023-05-08 13:45:29 -0400

    Use ChatGpt image creation feature; Create /image endpoint; Rename /chat to /text

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.CHATGPT_API_KEY
 });
+
 const openai = new OpenAIApi(configuration);
 
 // Set up the server
@@ -16,17 +17,31 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-// Set up the ChatGPT endpoint
-app.post("/chat", async (req, res) => {
+// Set up the ChatGPT text endpoint
+app.post("/text", async (req, res) => {
   // Get the prompt from the request
   const { prompt } = req.body;
 
-  // Generate a response with ChatGPT
+  // Generate a completion with ChatGPT
   const completion = await openai.createCompletion({
     model: "text-davinci-002",
     prompt: prompt,
   });
   res.send(completion.data.choices[0].text);
+});
+
+// Set up the ChatGPT image endpoint
+app.post("/image", async (req, res) => {
+  // Get the prompt from the request
+  const { prompt } = req.body;
+
+  // Generate an image with ChatGPT
+  const image = await openai.createImage({
+    n: 1,
+    size: "256x256",
+    prompt: prompt,
+  });
+  res.send(image.data.data[0].url);
 });
 
 // Start the server
